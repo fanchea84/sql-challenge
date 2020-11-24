a377e4369691bfb54d8f75802157612991fc77b8
Adam <70108536+fanchea84@users.noreply.github.com>
2020-11-23 16:21:53 -0800

    SQL code update

--- SQL_Homework_Code.sql
+++ SQL_Homework_Code.sql
@@ -60,9 +60,6 @@ JOIN departments ON dept_manager.dept_no = departments.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
--------------------------------------------------------------------------
--- Adam: there's duplication going on here. FIX IT.
--------------------------------------------------------------------------
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
